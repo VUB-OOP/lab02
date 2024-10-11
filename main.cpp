6426af184c5e4f060086cbabcdf054bc01a9904c
Krunoslav Husak <khusak@vub.hr>
2024-10-11 18:55:41 +0000

    Instanciranje objekta i prikaz podataka

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ public:
 };
 
 int main() {
-  std::cout << "Hello from VUB!" << std::endl;
+  Tocka2D tocka1 { 1.0, 2.0 };
+  Tocka2D tocka2 { 3.0, 4.0 };
+
+  std::cout << "Tocka 1: (" << tocka1.x << ", " << tocka1.y << ")" << std::endl;
+  std::cout << "Tocka 2: (" << tocka2.x << ", " << tocka2.y << ")" << std::endl;
 }
